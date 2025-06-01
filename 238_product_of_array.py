788b039cb79190e530309019de73403ec368865e
Aditya Surana <suranaaditya3@gmail.com>
2025-06-01 21:44:07 +0545

    238. Product of Array Except Self

---
+++ 238_product_of_array.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        ans = []
+        right_pref_mul = [1] * len(nums)
+        left_prod = 1
+        right_pref_mul[-1] = nums[-1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            right_pref_mul[i] = nums[i] * right_pref_mul[i + 1]
+
+        for i in range(len(nums)):
+            right_prod = 1 if i == len(nums) - 1 else right_pref_mul[i + 1]
+            ans.append(left_prod * right_prod)
+            left_prod *= nums[i]
+
+        return ans
+
+        """
+        ans = []
+        left_pref_mul = [1] * len(nums)
+        right_pref_mul = [1] * len(nums)
+        left_pref_mul[0] = nums[0]
+
+        left_pref_mul[0] = nums[0]
+
+        for i in range(len(nums)):
+            left_pref_mul[i] = nums[i] * left_pref_mul[i - 1]
+
+        right_pref_mul[-1] = nums[-1]
+        for i in range(len(nums) - 2, -1, -1):
+            right_pref_mul[i] = nums[i] * right_pref_mul[i + 1]
+
+        for i in range(len(nums)):
+            left_prod = 1 if i == 0 else left_pref_mul[i - 1]
+            right_prod = 1 if i == len(nums) - 1 else right_pref_mul[i + 1]
+            ans.append(left_prod * right_prod)
+
+        return ans
+        """
+
+
+s = Solution()
+result = s.productExceptSelf([-1, 1, 0, -3, 3])
+print(result)
