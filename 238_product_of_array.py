4b6ebb005403b9a6dff85126af26b92deb8f70bb
Aditya Surana <suranaaditya3@gmail.com>
2025-06-02 22:35:45 +0545

    optimized

--- 238_product_of_array.py
+++ 238_product_of_array.py
@@ -4,17 +4,16 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        ans = []
-        right_pref_mul = [1] * len(nums)
+        n = len(nums)
         left_prod = 1
-        right_pref_mul[-1] = nums[-1]
+        ans = [1] * n
 
         for i in range(len(nums) - 2, -1, -1):
-            right_pref_mul[i] = nums[i] * right_pref_mul[i + 1]
+            ans[i] = nums[i + 1] * ans[i + 1]
 
-        for i in range(len(nums)):
-            right_prod = 1 if i == len(nums) - 1 else right_pref_mul[i + 1]
-            ans.append(left_prod * right_prod)
+        for i in range(n):
+            right_prod = 1 if i == len(nums) - 1 else ans[i]
+            ans[i] = left_prod * right_prod
             left_prod *= nums[i]
 
         return ans
