66b0e5214313803959aac9252b78518d8b2a983e
Aditya Surana <suranaaditya3@gmail.com>
2025-06-02 23:39:52 +0545

    trapping rain water

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -3,7 +3,23 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-
+        low = 0
+        high = len(height) - 1
+        left_max = 0
+        right_max = 0
+        ans = 0
+        while low < high:
+            if height[low] < height[high]:
+                left_max = max(left_max, height[low])
+                ans += left_max - height[low]
+                low += 1
+            else:  # height[high] < height[low]
+                right_max = max(right_max, height[high])
+                ans += right_max - height[high]
+                high -= 1
+        return ans
+        ######################################## Prefix Max approach ########################################
+        """
         n = len(height)
         max_right = [0] * n
         max_left = [0] * n
@@ -22,6 +38,7 @@ class Solution:
             sum_ += min(max_left[i], max_right[i]) - height[i]
 
         return sum_
+        """
 
 
 s = Solution()
