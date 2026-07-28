389f5d9c70fbc58bb54d0c09db84841630873656
Aditya Surana <suranaaditya3@gmail.com>
2026-07-28 16:54:00 +0545

    104 maximam depth of binary tree using DFS

---
+++ binary-tree/104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,49 @@
+from typing import Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.height = 0
+
+    def helper(self, root: Optional[TreeNode], count: int) -> None:
+
+        if root is None:
+            self.height = max(self.height, count)
+            return
+        self.helper(root.left, count + 1)
+        self.helper(root.right, count + 1)
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.helper(root=root, count=0)
+        return self.height
+
+
+# root = [3, 9, 20, None, None, 15, 7]
+root = TreeNode(3)
+root1 = TreeNode(9)
+root2 = TreeNode(20)
+root3 = TreeNode(None)
+root4 = TreeNode(None)
+root5 = TreeNode(15)
+root6 = TreeNode(7)
+
+root.left = root1
+root.right = root2
+root2.left = root5
+root2.right = root6
+root1.left = None
+root1.right = None
+
+
+s = Solution()
+print(s.maxDepth(root))
